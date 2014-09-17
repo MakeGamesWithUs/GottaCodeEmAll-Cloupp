624afaa505dd7a81f0ebd3602f4a239ad5232a06
Dion Larson <me@dionlarson.com>
2014-09-16 17:05:16 -0700

    Spec for MyMonster.

---
+++ Source/MyMonster.swift
@@ -0,0 +1,37 @@
+//
+//  MyMonster.swift
+//  GottaCodeEmAll
+//
+//  Created by Dion Larson on 9/16/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class MyMonster: Monster {
+  
+  func addToBattle() {
+    
+  }
+  
+  func tackleAttack() {
+    
+  }
+  
+  func fireAttack() {
+    
+  }
+  
+  func waterAttack() {
+    
+  }
+  
+  func leafAttack() {
+    
+  }
+  
+  func swipeAttack(numberOfSwipes: Int) {
+    
+  }
+  
+}
