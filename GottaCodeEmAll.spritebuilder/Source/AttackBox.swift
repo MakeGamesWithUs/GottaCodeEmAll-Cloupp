66395650ab3c305e5c7986157095e04ff8f5983f
Gerald Monaco <gmonaco@Geralds-MacBook-Pro-2.local>
2014-09-29 21:34:01 -0700

    OS X Support

--- GottaCodeEmAll.spritebuilder/Source/AttackBox.swift
+++ GottaCodeEmAll.spritebuilder/Source/AttackBox.swift
@@ -16,6 +16,7 @@ class AttackButton: CCNodeColor {
     self.userInteractionEnabled = true
   }
   
+#if os(iOS)
   override func touchBegan(touch: UITouch, withEvent event: UIEvent) {
     println("Touched \(attackType.description())")
 //    var player = GameState.sharedInstance.player?
@@ -41,6 +42,7 @@ class AttackButton: CCNodeColor {
 //        println("\(attackType.description()) touched! This should not have happened...")
 //    }
   }
+#endif
   
 }
 
