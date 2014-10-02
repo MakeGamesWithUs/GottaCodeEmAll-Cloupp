ea766babb08afa68ec30066199edd9b3db388fff
Dion Larson <me@dionlarson.com>
2014-10-01 22:45:36 -0700

    Reset MyMonster.swift to empty again.

--- GottaCodeEmAll.spritebuilder/Source/MyMonster.swift
+++ GottaCodeEmAll.spritebuilder/Source/MyMonster.swift
@@ -1,49 +1,28 @@
 
 class MyMonster: Monster {
   
-  // addToBattle goes below this
   
-  override func addToBattle() {
-    element = MonsterElement.Water
-    nickname = "Morty"
-  }
+  // addToBattle goes below this
   
   // addToBattle goes above this
   
   
   
   // tackleMove goes below this
-  
-  override func tackleMove() {
-    self.performTackle()
-  }
-  
+
   // tackleMove goes above this
   
   
   
   // elementalMove goes below this
-  
-  override func elementalMove() {
-    if opponent.weakAgainst == self.element {
-      self.performElemental()
-    } else {
-      self.performTackle()
-    }
-  }
-  
+
   // elementalMove goes above this
   
   
   
   // swipeMove goes below this
   
-  override func swipeMove(numberOfSwipes: Int) {
-    for _ in 1...numberOfSwipes {
-      self.performSwipe()
-    }
-  }
-  
   // swipeMove goes above this
+  
   
 }
