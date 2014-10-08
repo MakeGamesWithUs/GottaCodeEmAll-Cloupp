1fe3fefab9d858927d913d7b34f0c9d8b71e5e1f
Dion Larson <me@dionlarson.com>
2014-10-08 15:02:56 -0700

    Fixing indentation in MyMonster.swift to be 4 spaces.

--- GottaCodeEmAll.spritebuilder/Source/MyMonster.swift
+++ GottaCodeEmAll.spritebuilder/Source/MyMonster.swift
@@ -1,36 +1,36 @@
 class MyMonster: Monster
 {
-  
-  
+    
+    
     // addToBattle goes below this
-  
-  
-  
+    
+    
+    
     // addToBattle goes above this
-  
-  
-  
+    
+    
+    
     // tackleMove goes below this
-  
-  
-
+    
+    
+    
     // tackleMove goes above this
-  
-  
-  
+    
+    
+    
     // elementalMove goes below this
-  
-  
-
+    
+    
+    
     // elementalMove goes above this
-  
-  
-  
+    
+    
+    
     // swipeMove goes below this
-  
-  
-  
+    
+    
+    
     // swipeMove goes above this
-  
-  
+    
+    
 }
