d55d3f85653649f53fe261c46f600cb0d80f4610
Dion Larson <me@dionlarson.com>
2014-11-04 19:14:36 -0800

    Changes to comments in MyMonster.

--- GottaCodeEmAll.spritebuilder/Source/MyMonster.swift
+++ GottaCodeEmAll.spritebuilder/Source/MyMonster.swift
@@ -1,46 +1,41 @@
 class MyMonster: Monster
 {
-  
-  func initialize()
-  {
-    nickname = "Morty"
-  }
-  
-  func setupMonsterStats()
-  {
-    element = "None"
-    level = 1
-  }
-  
-  // tackleButtonPressed goes below this
-  
-  
-  
-  // tackleButtonPressed goes above this
-  
-  
-  
-  // elementalButtonPressed goes below this
-  
-  
-  
-  // elementalButtonPressed goes above this
-  
-  
-  
-  // powerupButtonPressed goes below this
-  
-  
-  
-  // powerupButtonPressed goes above this
-  
-  
-  
-  // singButtonPressed goes below this
-  
-  
-  
-  // singButtonPressed goes above this
-  
-  
+    
+    func initialize()
+    {
+        // The code in this function runs first
+        
+        nickname = "Morty"
+    }
+    
+    func setupMonsterStats()
+    {
+        element = "None"
+        level = 1
+    }
+    
+    
+    // tackleButtonPressed goes below this
+    
+    
+    
+    // tackleButtonPressed goes above this
+    
+    
+    
+    // elementalButtonPressed goes below this
+    
+    
+    
+    // elementalButtonPressed goes above this
+    
+    
+    
+    // singButtonPressed goes below this
+    
+    
+    
+    // singButtonPressed goes above this
+    
+    
 }
